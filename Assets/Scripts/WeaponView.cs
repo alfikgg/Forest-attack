425102d842c76e21c3bcdfc37e70a9428ab994de
alfikgg <alfikgg@gmail.com>
2021-04-26 15:35:39 +0300

    add shop

---
+++ Assets/Scripts/WeaponView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _label;
+    [SerializeField] private TMP_Text _price;
+    [SerializeField] private Image _icon;
+    [SerializeField] private Button _buyButton;
+
+    private Weapon _weapon;
+
+    public void Render(Weapon weapon)
+    {
+        _weapon = weapon;
+        
+    }
+}
