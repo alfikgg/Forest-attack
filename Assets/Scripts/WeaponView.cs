cea7f133aa8096c2e4caee4f1977b3fe343ae2ae
alfikgg <alfikgg@gmail.com>
2021-04-28 22:39:01 +0300

    update

--- Assets/Scripts/WeaponView.cs
+++ Assets/Scripts/WeaponView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class WeaponView : MonoBehaviour
@@ -9,13 +10,32 @@ public class WeaponView : MonoBehaviour
     [SerializeField] private TMP_Text _label;
     [SerializeField] private TMP_Text _price;
     [SerializeField] private Image _icon;
-    [SerializeField] private Button _buyButton;
+    [SerializeField] private Button _sellButton;
 
     private Weapon _weapon;
+
+    public event UnityAction<Weapon, WeaponView> SellButtonClick;
+
+    private void OnEnable()
+    {
+        _sellButton.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _sellButton.onClick.RemoveListener(OnButtonClick);
+    }
 
     public void Render(Weapon weapon)
     {
         _weapon = weapon;
-        
+        _label.text = weapon.Laber;
+        _price.text = weapon.Price.ToString();
+        _icon.sprite = weapon.Icon;        
+    }
+
+    private void OnButtonClick()
+    {
+        SellButtonClick?.Invoke(_weapon, this);
     }
 }
