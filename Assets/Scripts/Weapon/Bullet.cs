5c0da3d5734220240ae9e746ba67202490a4514f
alfikgg <alfikgg@gmail.com>
2021-05-05 22:22:41 +0300

    update bullet

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -6,10 +6,23 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private int _damage;
     [SerializeField] private float _speed;
+    [SerializeField] private int _flighRange = 0;
 
+    private float _startPositionX;
+
+    private void Start()
+    {
+        _startPositionX = transform.position.x;
+    }
     private void Update()
     {
         transform.Translate(Vector2.left * _speed * Time.deltaTime);
+        if (_flighRange != 0)
+        {
+            float maxDistance = Mathf.Abs(_startPositionX - transform.position.x);
+            if (maxDistance >= _flighRange)
+                Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,6 +33,7 @@ public class Bullet : MonoBehaviour
         }
         else if (collision.gameObject.TryGetComponent(out Bullet bullet))
             return;
+     
         Destroy(gameObject);
     }
 }
