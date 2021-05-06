b64e155cad7a8b06a2ebf2109b93b81a6f06a070
alfikgg <alfikgg@gmail.com>
2021-05-06 09:40:27 +0300

    update shotgun

--- Assets/Scripts/Weapon/ShotGun.cs
+++ Assets/Scripts/Weapon/ShotGun.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class ShotGun : Weapon
 {
+    [SerializeField] private int _spreadAngle;
+    [SerializeField] private int _numberBulletsInShot = 1;
+
     public override void Shoot(Transform shootPoint)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < _numberBulletsInShot; i++)
         {
-            var rotationZ = Quaternion.AngleAxis(Random.Range(-5, 5), transform.forward);
+            var rotationZ = Quaternion.AngleAxis(Random.Range(-_spreadAngle / 2, _spreadAngle / 2), transform.forward);
             Instantiate(Bullet, shootPoint.position, rotationZ);
         }
     }
